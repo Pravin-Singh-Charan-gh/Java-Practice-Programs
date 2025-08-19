71fe1e6f991f94bf9453e23a155d3912ea7ea565
Pravin Singh Charan <125742995+Pravin-Singh-Charan-gh@users.noreply.github.com>
2025-08-19 23:33:46 +0530

    Create Input.java

---
+++ Basic/Input.java
@@ -0,0 +1,13 @@
+package Basic;
+// package Basic;
+
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n=sc.nextInt();
+        System.out.println("You entered : "+ n);
+    }
+}
